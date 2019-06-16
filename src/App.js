5c2e60fa8266f83a1880a60e3043accc9082865c
Gloria Chen <cmxgloria@gmail.com>
2019-06-16 20:41:22 +1000

    fixed setState function

--- src/App.js
+++ src/App.js
@@ -22,13 +22,22 @@ class App extends React.Component {
     const data = await api_call.json();
     if (city && country) {
       console.log(data);
-      this.setState = ({
+      this.setState({
         temperature: data.main.temp,
         city: data.name,
         country: data.sys.country,
         humidity: data.main.humidity,
-        description: data.weather[0].description,
+        description: data.weather.description,
         error: ''
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'Please enter the value'
       });
     }
   }
