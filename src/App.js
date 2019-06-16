e38c6a05db5eebe0819ac6df44cdff9d678fd7ec
Gloria Chen <cmxgloria@gmail.com>
2019-06-16 10:53:51 +1000

    added city n country

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = '1fc53b3deb63dd790005876d80960b0e';
+
+class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather />
+      </div>
+
+    );
+  }
+}
+export default App;
+
