67a6d68a6d2804fcc9e097917f3102319e27c460
Gloria Chen <cmxgloria@gmail.com>
2019-06-16 21:07:16 +1000

    fixed humidity

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,7 @@ const Weather = props => (
   <div>
     {props.city && props.country && <p>Location: {props.city}, {props.country}</p>}
     {props.temperature && <p>Temperature: {props.temperature}</p>}
-    {props.humidify && <p>Humidify: {props.humidify}</p>}
+    {props.humidity && <p>Humidity: {props.humidity}</p>}
     {props.description && <p>Condition: {props.description}</p>}
     {props.error && <p>{props.error}</p>}
   </div>
