a5196013c810221d210dde92bcb26aaa2067e098
Gloria Chen <cmxgloria@gmail.com>
2019-06-16 12:04:58 +1000

    added props

--- src/App.js
+++ src/App.js
@@ -6,20 +6,45 @@ import Weather from './components/Weather';
 const API_KEY = '1fc53b3deb63dd790005876d80960b0e';
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
-    console.log(data);
+    if (city && country) {
+      console.log(data);
+      this.setState = ({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+      });
+    }
   }
   render() {
     return (
       <div>
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
 
     );
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,10 @@ class Weather extends React.Component {
   render() {
     return (
       <div>
-        Weather Component!
+        {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+        {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+        {this.props.humidify && <p>Humidify: {this.props.humidify}</p>}
+        {this.props.description && <p>Condition: {this.props.description}</p>}
       </div>
     );
   }
